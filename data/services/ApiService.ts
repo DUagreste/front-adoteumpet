b7fce069bf03e72a64c3a399c7dda21dc89c1249
Vítor Pinheiro <vitor.pinheiro97@hotmail.com>
2022-06-14 17:09:09 -0300

    test baseURL

--- data/services/ApiService.ts
+++ data/services/ApiService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const ApiService = axios.create({
-    baseURL: 'http://back-adoteumpet.herokuapp.com/api',
+    baseURL: 'https://back-adoteumpet.herokuapp.com/api',
     headers: {
         'Content-Type': 'application/json'
     }
