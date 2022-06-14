4413d07a724711b2191be09e26aaf41333e21ef9
Vítor Pinheiro <vitor.pinheiro97@hotmail.com>
2022-06-14 16:21:43 -0300

    upgrade backend

--- data/services/ApiService.ts
+++ data/services/ApiService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const ApiService = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: 'back-adoteumpet.herokuapp.com/api/',
     headers: {
         'Content-Type': 'application/json'
     }
